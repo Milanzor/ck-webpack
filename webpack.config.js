1b3fc88bda5c878c66f4b9ad2ec9bd9f7750ed3f
Milan <milanvanas@gmail.com>
2017-12-17 20:37:53 +0100

    Added the webpack file

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WildcardsEntryWebpackPlugin = require('wildcards-entry-webpack-plugin');
+const webpack = require('webpack');
+
+// Initialize the extract plugin to extract css to a different file
+const extractPlugin = new ExtractTextPlugin({
+    filename: '[name].min.css'
+});
+
+// Config
+module.exports = {
+
+    // Entry
+    entry: WildcardsEntryWebpackPlugin.entry(path.resolve(__dirname, 'entry/**/*.js')),
+
+    // Output
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../private_html', 'assets'),
+        publicPath: ''
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, "lib"), "node_modules"]
+
+    },
+    // Module
+    module: {
+        rules: [
+
+            // SCSS rule
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+
+            // Babel rule
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+            // Provide plugin, jquery
+            {
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                }, {
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            },
+
+            // Font rules
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'fonts/[name].[ext]'
+                    }
+                },
+            }
+
+        ],
+    },
+
+    // Plugins
+    plugins: [
+        extractPlugin,
+        new WildcardsEntryWebpackPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "commons",
+            filename: "commons.js",
+            minChunks: 2
+        })
+    ],
+
+    // Devserver
+    devServer: {
+        contentBase: path.join(__dirname, 'private_html'),
+        compress: true,
+        open: true,
+        port: 8080
+    }
+};
